23ad5e47e68ef25da524e1e648436351fd2d6539
fankai <2214507843@qq.com>
2023-03-21 16:39:31 +0800

    master commit test2

--- src/main/java/com/fk/GitTest.java
+++ src/main/java/com/fk/GitTest.java
@@ -2,20 +2,6 @@ package com.fk;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println(4);
-        System.out.println(5);
-         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println(4);
-        System.out.println(5);
-         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println(4);
-        System.out.println(5);
+
     }
 }
