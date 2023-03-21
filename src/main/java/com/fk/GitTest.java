99e819be5235484bb8f7b806d34f22cd09540822
fankai <2214507843@qq.com>
2023-03-21 14:39:38 +0800

    hot fix push ssh

--- src/main/java/com/fk/GitTest.java
+++ src/main/java/com/fk/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hot fix commit3");
         System.out.println("master commit3");
         System.out.println("push test");
+        System.out.println("push ssh test");
     }
 }
