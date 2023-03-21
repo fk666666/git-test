ba1d33470285bb8c515257b108d2d9e8775b2a23
fankai <2214507843@qq.com>
2023-03-21 11:33:18 +0800

    master  commit3

--- src/main/java/com/fk/GitTest.java
+++ src/main/java/com/fk/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println(5);
         System.out.println("hot fix commit1");
         System.out.println("master commit1");
+        System.out.println("master commit2");
     }
 }
