302c771555d99c2c98a6c9617008ebb3f54608f1
fankai <120454989+fk666666@users.noreply.github.com>
2023-03-21 16:42:38 +0800

    Update GitTest.java

--- src/main/java/com/fk/GitTest.java
+++ src/main/java/com/fk/GitTest.java
@@ -2,6 +2,19 @@ package com.fk;
 
 public class GitTest {
     public static void main(String[] args) {
-
+         System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
+        System.out.println("hot fix commit1");
+        System.out.println("master commit1");
+        System.out.println("master commit2");
+        System.out.println("hot fix commit2");
+        System.out.println("hot fix commit3");
+        System.out.println("master commit3");
+        System.out.println("push test");
+        System.out.println("push ssh test");
+        System.out.println("pull  test");
     }
 }
