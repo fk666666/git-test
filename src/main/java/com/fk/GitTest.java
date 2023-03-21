002f88c18710de9a40d89fc1929f4a9090513513
fankai <120454989+fk666666@users.noreply.github.com>
2023-03-21 16:37:50 +0800

    Update GitTest.java

--- src/main/java/com/fk/GitTest.java
+++ src/main/java/com/fk/GitTest.java
@@ -7,5 +7,15 @@ public class GitTest {
         System.out.println(3);
         System.out.println(4);
         System.out.println(5);
+         System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
+         System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
     }
 }
