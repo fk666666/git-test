30e408fc25c47db18e3338a54d5a0181e4332be9
fankai <2214507843@qq.com>
2023-04-12 16:54:23 +0800

    53t

--- src/main/java/com/fk/GitTest.java
+++ src/main/java/com/fk/GitTest.java
@@ -7,15 +7,5 @@ public class GitTest {
         System.out.println(3);
         System.out.println(4);
         System.out.println(5);
-        System.out.println("hot fix commit1");
-        System.out.println("master commit1");
-        System.out.println("master commit2");
-        System.out.println("hot fix commit2");
-        System.out.println("hot fix commit3");
-        System.out.println("master commit3");
-        System.out.println("push test");
-        System.out.println("push ssh test");
-        System.out.println("pull  test");
-        System.out.println("pull  test");
     }
 }
